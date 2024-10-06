cf349e5400315df0d49004d57c3bb10c0d22eb5d
Abu Huraira Khan <abuhurairakhan3127@gmail.com>
2024-10-07 00:51:12 +0600

    remove backend

--- backend/route/randRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-import { getUserFriendsSuggest } from '../controller/randController.js';
-
-router.get('/friends-suggest',getUserFriendsSuggest);
-
-export default router;
